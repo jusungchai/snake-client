ed764d6ce5a44f985531b48dda522d98318ef064
Jay Chai <chunsaa@gmail.com>
2019-10-22 01:08:38 +0000

    experimented with move commands

--- client.js
+++ client.js
@@ -18,6 +18,9 @@ const connect = function () {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write('Name: JSC');
+    /* setInterval(() => {      
+      conn.write('Move: up');
+    }, 50);  */   
   });
 
   return conn;
