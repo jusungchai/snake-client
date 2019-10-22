f63502acfb2be0d316bd65a20a7b6cc35cdbf865
Jay Chai <chunsaa@gmail.com>
2019-10-22 01:29:41 +0000

    added user input handler and included input setup for players

--- play.js
+++ play.js
@@ -1,8 +1,31 @@
 const net = require('net');
 
 /**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+}
+
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+/**
  * Establishes connection with the game server
  */
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+setupInput();
