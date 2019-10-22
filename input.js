3cb9da4e77a833a422d316a8f9e15c7b3580073e
Jay Chai <chunsaa@gmail.com>
2019-10-22 02:15:21 +0000

    added taunt message to other players

--- input.js
+++ input.js
@@ -29,6 +29,9 @@ const handleUserInput = function (key) {
   if (key === "d"){
     connection.write('Move: right');    
   }
+  if (key === "t"){
+    connection.write('Say: YOLO');
+  }
 }
 
 module.exports = { setupInput };
