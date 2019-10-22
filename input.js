fe462fcfc47fa8486292733c0e56a1ca04a2bb4c
Jay Chai <chunsaa@gmail.com>
2019-10-22 02:11:26 +0000

    added connection to server and wasd movements

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
@@ -13,6 +16,18 @@ const handleUserInput = function (key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === "w"){
+    connection.write('Move: up');    
+  }
+  if (key === "a"){
+    connection.write('Move: left');    
+  }
+  if (key === "s"){
+    connection.write('Move: down');    
+  }
+  if (key === "d"){
+    connection.write('Move: right');    
   }
 }
 
